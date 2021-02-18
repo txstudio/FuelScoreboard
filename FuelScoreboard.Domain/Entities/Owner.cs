e69ce0a470769669c871e69852debe51102f97e0
Shi Hao Wang <st890409@hotmail.com>
2021-02-18 16:40:22 +0800

    Add Owner Enttiy

---
+++ FuelScoreboard.Domain/Entities/Owner.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuelScoreboard.Domain.Entities
+{
+    /// <summary>
+    /// 擁有者
+    /// </summary>
+    public class Owner
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public DateTime? Birth { get; set; }
+
+        public IList<Vehicle> Vehicles { get; private set; }
+    }
+}
