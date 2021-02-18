04781b215f16addf72a7ef444b77df80bc9ad66e
Shi Hao Wang <st890409@hotmail.com>
2021-02-18 16:13:59 +0800

    Add Vehicle entity

---
+++ FuelScoreboard.Domain/Entities/Vehicle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FuelScoreboard.Domain.Entities
+{
+    /// <summary>
+    /// 車輛
+    /// </summary>
+    public class Vehicle
+    {
+        public Vehicle()
+        {
+            this.FillUpLogs = new List<FillUpLog>();
+        }
+
+        /// <summary>
+        /// 車牌號碼
+        /// </summary>
+        public string RegistrationPlate { get; set; }
+
+        public string Note { get; set; }
+
+        public IList<FillUpLog> FillUpLogs { get; private set; }
+    }
+}
