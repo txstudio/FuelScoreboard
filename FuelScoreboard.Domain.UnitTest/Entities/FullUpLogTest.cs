a8572b5a130657386cda9ac7e9f65dd3cbc04747
Shi Hao Wang <st890409@hotmail.com>
2021-02-18 15:26:13 +0800

    Add fill up log entities and unit test

---
+++ FuelScoreboard.Domain.UnitTest/Entities/FullUpLogTest.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using FuelScoreboard.Domain.Entities;
+using NUnit.Framework;
+
+namespace FuelScoreboard.Domain.UnitTest.Entities
+{
+    public sealed class FullUpLogTest
+    {
+        [Test]
+        public void KmPerLiterShouldReturnNullWhenMeterIsNull()
+        {
+            var _fillUpLog = new FillUpLog();
+
+            _fillUpLog.Meter = null;
+
+            _fillUpLog.KmPerLiter.Should().BeNull();
+        }
+
+        [Test]
+        public void KmPerLiterShouldReturnNullWhenLiterIsNull()
+        {
+            var _fillUpLog = new FillUpLog();
+
+            _fillUpLog.Meter = 600;
+            _fillUpLog.Liter = null;
+
+            _fillUpLog.KmPerLiter.Should().BeNull();
+        }
+
+        [Test]
+        public void KmPerLiterShoudReturnCorrectValueWhenMeterAndLiterHasValue()
+        {
+            var _fillUpLog = new FillUpLog();
+
+            _fillUpLog.Meter = 487M;
+            _fillUpLog.Liter = 34.7M;
+
+            var _kmPerLiterValue = 14.03M;
+
+            _fillUpLog.KmPerLiter.Should().Be(_kmPerLiterValue);
+        }
+    }
+}
---
+++ FuelScoreboard.Domain/Entities/FillUpLog.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace FuelScoreboard.Domain.Entities
+{
+    /// <summary>
+    /// 加油紀錄
+    /// </summary>
+    public class FillUpLog
+    {
+        /// <summary>
+        /// 時間
+        /// </summary>
+        public DateTime? EventDate { get; set; }
+
+        /// <summary>
+        /// 總里程
+        /// </summary>
+        public decimal? Odometer { get; set; }
+
+        /// <summary>
+        /// 公升
+        /// </summary>
+        public decimal? Liter { get; set; }
+
+        /// <summary>
+        /// 行駛里程
+        /// </summary>
+        public decimal? Meter { get; set; }
+
+        /// <summary>
+        /// 金額
+        /// </summary>
+        public decimal? Money { get; set; }
+
+        /// <summary>
+        /// 公里/公升
+        /// </summary>
+        public decimal? KmPerLiter 
+        { 
+            get 
+            {
+                if (this.Meter.HasValue == false)
+                    return null;
+
+                if (this.Liter.HasValue == false)
+                    return null;
+
+                return Math.Round(this.Meter.Value / this.Liter.Value, 2);
+            } 
+        }
+    }
+}
